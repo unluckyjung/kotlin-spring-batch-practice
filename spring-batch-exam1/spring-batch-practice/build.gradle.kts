7e7090b0108408491be6f79c6bbf3b6c945e5ac2
unluckyjung <ybook2012@gmail.com>
2022-12-13 01:07:39 +0900

    chore: spring batch 의존성 추가

--- spring-batch-exam1/spring-batch-practice/build.gradle.kts
+++ spring-batch-exam1/spring-batch-practice/build.gradle.kts
@@ -24,6 +24,7 @@ allOpen {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
--- spring-batch-exam1/spring-batch-practice/src/main/kotlin/com/example/springbatchpractice/SpringBatchPracticeApplication.kt
+++ spring-batch-exam1/spring-batch-practice/src/main/kotlin/com/example/springbatchpractice/SpringBatchPracticeApplication.kt
@@ -1,8 +1,10 @@
 package com.example.springbatchpractice
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@EnableBatchProcessing
 @SpringBootApplication
 class SpringBatchPracticeApplication
 
