16e01aecc61b704bd87d172d9b0acc61ec18c751
unluckyjung <ybook2012@gmail.com>
2022-12-13 02:10:17 +0900

    feat: job param 을 통해서 배치 작업을 구분하는 예시 구현

--- spring-batch-exam1/spring-batch-practice/src/main/kotlin/com/example/springbatchpractice/batch/SampleTaskletJobConfig.kt
+++ spring-batch-exam1/spring-batch-practice/src/main/kotlin/com/example/springbatchpractice/batch/SampleTaskletJobConfig.kt
@@ -4,8 +4,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -19,19 +21,35 @@ class SampleTaskletJobConfig(
     @Bean
     fun sampleTaskletJob(): Job {
         return jobBuilderFactory.get("sampleTaskletJob")
-            .start(sampleTaskletJobStep1()).build()
+            .start(sampleTaskletJobStep1())
+            .next(sampleTaskletJobStep2("defaultDate"))
+            .build()
     }
 
     @Bean
     fun sampleTaskletJobStep1(): Step {
-        return stepBuilderFactory.get("sampleTaskletJobStep1")
+        return stepBuilderFactory.get(JOB_NAME)
             .tasklet { _, _ ->
+                log.info(">>>>>> this is Step1")
                 log.info("-> job -> [step1]")
                 RepeatStatus.FINISHED
             }.build()
     }
 
+    @Bean
+    @JobScope
+    fun sampleTaskletJobStep2(@Value("#{jobParameters[date]}") date: String): Step {
+        return stepBuilderFactory.get("sampleTaskletJobStep2")
+            .tasklet { _, _ ->
+                log.info(">>>>>> this is Step2")
+                log.info("-> job -> step1 -> [step2]")
+                log.info("date: $date")
+                RepeatStatus.FINISHED
+            }.build()
+    }
+
     companion object {
+        private const val JOB_NAME = "sampleTaskletJob"
         private val log = LoggerFactory.getLogger(this::class.java)
     }
 }
